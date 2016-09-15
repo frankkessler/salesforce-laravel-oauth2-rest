97ec89f5f3010c9668bc762ff646874d316b9a22
Frank Kessler <frankkessler@gmail.com>
2016-09-15 17:00:56 -0400

    Add helper unit tests

---
+++ tests/HelperTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
+class HelperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEnv()
+    {
+        putenv('var_true=true');
+        putenv('var_false=false');
+
+        $this->assertSame(true, env('var_true'));
+        $this->assertSame(false, env('var_false'));
+        $this->assertSame('', env('var_empty'));
+        $this->assertSame(null, env('var_null'));
+
+        $this->assertSame(true, env('var_true_parentheses'));
+        $this->assertSame(false, env('var_false_parentheses'));
+        $this->assertSame('', env('var_empty_parentheses'));
+        $this->assertSame(null, env('var_null_parentheses'));
+    }
+}
