ac960e51fab49d7aad7e9b3548d2913055ed1a2d
Frank Kessler <frankkessler@gmail.com>
2016-09-14 20:57:53 -0400

    Add authentication url test

---
+++ tests/AuthenticationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
+class AuthenticationTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
+{
+    public function testAuthenticationUrl()
+    {
+        $url = \Frankkessler\Salesforce\Authentication::returnAuthorizationLink();
+
+        $expected_url_html = '<a href="https://login.salesforce.com/services/oauth2/authorize?response_type=code&access_type=offline&client_id=&redirect_uri=&scope=">Login to Salesforce</a>';
+        $this->assertEquals($expected_url_html,$url);
+    }
+}
