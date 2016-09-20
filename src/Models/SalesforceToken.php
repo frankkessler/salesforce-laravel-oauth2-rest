5dcc97dd65ffd932e4876f18e69b81c9e9aa2644
Frank Kessler <frankkessler@gmail.com>
2016-09-20 14:21:45 -0400

    Use DateTimeInterface in property definition to allow for more types of values

--- src/Models/SalesforceToken.php
+++ src/Models/SalesforceToken.php
@@ -6,15 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
+ * Salesforce Token Model to store salesforce oauth tokens in a database via Eloquent
+ *
  * @property integer $id
  * @property string $access_token
  * @property string $refresh_token
  * @property string $instance_base_url
  * @property integer $user_id
- * @property \DateTime $expires
- * @property \DateTime $created_at
- * @property \DateTime $updated_at
- * @property \DateTime $deleted_at
+ * @property \DateTimeInterface $expires
+ * @property \DateTimeInterface $created_at
+ * @property \DateTimeInterface $updated_at
+ * @property \DateTimeInterface $deleted_at
  */
 
 class SalesforceToken extends Model
