6edbe31a34c5b14269b0b8eb9fc8881495a334fc
Frank Kessler <frankkessler@gmail.com>
2016-09-28 23:19:47 -0400

    Update sobject response

--- src/Responses/Sobject/SobjectGetResponse.php
+++ src/Responses/Sobject/SobjectGetResponse.php
@@ -13,7 +13,7 @@ class SobjectGetResponse extends SalesforceBaseResponse
 
     public function __construct($data)
     {
-        $this->sobject = json_decode($data['raw_body']);
+        $this->sobject = json_decode($data['raw_body'], true);
 
         parent::__construct($data);
     }
